a1552ae87e88a3d388c44ee6fe11141cd4fa9437
YesVladess <vladess444@yandex.ru>
2022-05-13 23:05:46 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by YesVladess on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
